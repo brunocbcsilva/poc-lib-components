cdfc39567fc563f4518e0caa6442abd7cb74f8d8
bruno.cesar <bruno.cesar@doc88.com.br>
2024-05-15 16:13:39 -0300

    criando componentes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { defineCustomElement } from 'vue'
 
 import ChatCard from './components/ChatCard.ce.vue'
+import HeaderComponent from './components/HeaderComponent.ce.vue'
+import ComercLogo from '@/components/ComercLogo.ce.vue'
 
 const componentsConfig = [
-  { tagName: 'cm-chat-card', component: ChatCard},
+  { tagName: 'cm-chat-card', component: ChatCard },
+  { tagName: 'comerc-top-header', component: HeaderComponent },
+  { tagName: 'comerc-logo', component: ComercLogo }
 ]
 
 componentsConfig.forEach((config) => {
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({ customElement: true }),
-    VueDevTools()
-  ],
+  plugins: [vue({ customElement: true }), VueDevTools()],
   define: {
     'process.env': process.env
   },
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: 'src/index.js',
+      entry: resolve(__dirname, 'src/index.js'),
       name: 'ComercLib',
       fileName: 'comerc-lib'
     },
     sourcemap: true,
     target: 'esnext',
-    minify: false,
+    minify: false
   },
   resolve: {
     alias: {
